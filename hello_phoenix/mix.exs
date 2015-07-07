8f5a5221ad43657799d04d0e56a9f60a4308e7c3
Marlus Saraiva <marlus.saraiva@gmail.com>
2015-07-07 01:40:56 -0300

    Update hello_phoenix to use erlang 18

--- hello_phoenix/mix.exs
+++ hello_phoenix/mix.exs
@@ -32,6 +32,6 @@ defmodule HelloPhoenix.Mixfile do
      {:phoenix_html, "~> 1.0"},
      {:phoenix_live_reload, "~> 0.4", only: :dev},
      {:cowboy, "~> 1.0"},
-     {:exrm, "~> 0.15.3"}]
+     {:exrm, "~> 0.18.1"}]
   end
 end
